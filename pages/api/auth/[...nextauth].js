d0469ca96cfa0d716ce296244484d60e15d2f4c9
belfortep <pbelforte2003@gmail.com>
2022-04-06 10:40:30 -0300

    agregado mongodbadapter

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
+import clientPromise from 'lib/mongodb'
 export default NextAuth({
+
+    adapter: MongoDBAdapter(clientPromise),
 
     providers: [    //voy a poder iniciar sesion con los providers que aca escriba
 
